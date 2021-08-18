4c5de9a90f9700190946e6b68fdb18ac47aab9ee
QuentinBeeckmans <50259468+QuentinBeeckmans@users.noreply.github.com>
2021-08-18 17:49:57 +0200

    ListEvent : displaying, placeholder, new informations, padding and margin clean

--- lib/pages/ListEventsScreen.dart
+++ lib/pages/ListEventsScreen.dart
@@ -1,13 +1,13 @@
 import 'dart:developer';
+import 'dart:js';
 
 import 'package:artnext/models/event.dart';
 import 'package:artnext/pages/common/MyDrawer.dart';
 import 'package:artnext/pages/events/DisplayEvenementScreen.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer';
-
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'events/CreateEvenementScreen.dart';
 
 export 'ListEventsScreen.dart';
@@ -21,11 +21,38 @@ class ListEventsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Events'),
       ),
-      body: StreamBuilder(stream: FirebaseFirestore.instance
-            .collection('events')
-            .orderBy('startDate', descending: true)
-            .snapshots(),
-        builder: buildEventsList,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top:15.0, bottom:10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Filter by'),
+                ),
+                SizedBox(width: 30),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Sorted by'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 200.0,
+              child: StreamBuilder(stream: FirebaseFirestore.instance
+                    .collection('events')
+                    .orderBy('startDate', descending: true)
+                  .where('startDate', isGreaterThan: DateTime.now())
+                    .snapshots(),
+                builder: buildEventsList,
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -38,40 +65,107 @@ class ListEventsScreen extends StatelessWidget {
   }
 }
 
+String readTimestamp(int timestamp) {
+  initializeDateFormatting('fr_CH', null);
+  var now = new DateTime.now();
+  var format = new DateFormat('dd/MM/yyy');
+  var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000);
+  var diff = date.difference(now);
+  var time = '';
+  var timer = format.format(date);
+
+  // if (diff.inSeconds <= 0 || diff.inSeconds > 0 && diff.inMinutes == 0 || diff.inMinutes > 0 && diff.inHours == 0 || diff.inHours > 0 && diff.inDays == 0) {
+  //   time = format.format(date);
+  // } else {
+  //   if (diff.inDays == 1) {
+  //     time = diff.inDays.toString() + 'DAY AGO';
+  //   } else {
+  //     time = diff.inDays.toString() + 'DAYS AGO';
+  //   }
+  // }
+
+  return timer;
+}
+String readTimestamp2(int timestamp) {
+  initializeDateFormatting('fr_CH', null);
+  var now = new DateTime.now();
+  var format = new DateFormat('yyy');
+  var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000);
+  var diff = date.difference(now);
+  var time = '';
+  var timer = format.format(date);
+
+  // if (diff.inSeconds <= 0 || diff.inSeconds > 0 && diff.inMinutes == 0 || diff.inMinutes > 0 && diff.inHours == 0 || diff.inHours > 0 && diff.inDays == 0) {
+  //   time = format.format(date);
+  // } else {
+  //   if (diff.inDays == 1) {
+  //     time = diff.inDays.toString() + 'DAY AGO';
+  //   } else {
+  //     time = diff.inDays.toString() + 'DAYS AGO';
+  //   }
+  // }
+
+  return timer;
+}
+
 Widget buildEventsList(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
   if (snapshot.hasData) {
-    return ListView.builder(
-        itemCount: snapshot.data!.docs.length,
-        itemBuilder: (context, index) {
-          DocumentSnapshot eventFromFirebase = snapshot.data!.docs[index];
-          //log(event.reference.id);
-          Event event = Event.fromJson(eventFromFirebase);
-          log("ListEventsScreen - buildEventsList - event #" +
-              index.toString() +
-              " = " +
-              event.id);
-          return ListTile(
+    return Column(
+      children: <Widget>[
+        Text(readTimestamp2(Event.fromJson(snapshot.data!.docs[0]).startDate.millisecondsSinceEpoch)),
+        Expanded(
+          child: ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot eventFromFirebase = snapshot.data!.docs[index];
+                //log(event.reference.id);
+                Event event = Event.fromJson(eventFromFirebase);
+                // log("ListEventsScreen - buildEventsList - event #" +
+                //     index.toString() +
+                //     " = " +
+                //     event.id);
 
-            title: Text(event.title),
-            subtitle: Text(event.city),
-            // TODO décommenter pour remettre l'image
-            /*leading: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                child:
-                FadeInImage(
-                  image: NetworkImage((event.image.contains("http") ? event.image : "https://cdn1.iconfinder.com/data/icons/business-company-1/500/image-512.png")),
-                  placeholder: AssetImage('https://cdn1.iconfinder.com/data/icons/business-company-1/500/image-512.png'),
-                  // CachedNetworkImage(
-                  //     placeholder: (context, url) => CircularProgressIndicator(),
-                  //     imageUrl: event.image,
-                  // ),
-                )),*/
-            onTap: () => {
-              Navigator.pushNamed(context, DisplayEvenementScreen.routeName,
-                  arguments: event)
-            },
-          );
-        });
+                var Datum = readTimestamp(event.startDate.millisecondsSinceEpoch);
+
+                return ListTile(
+                  title: Text(event.title),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 3.0),
+                    child: Text( (event.details.length > 100) ? event.details.substring(0,100)+("[...]") : event.details),
+                  ),
+                  leading:  SizedBox(
+                    height: 100.0,
+                    width: 100.0,
+                    child: ClipRRect(
+                        // borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          child: FadeInImage(
+                            image: NetworkImage((event.image.contains("http") ? event.image : "assets/images/placeholder.jpg")),
+                            placeholder: AssetImage('assets/images/placeholder.jpg'),
+                          )),
+                  ),
+
+                  onTap: () => {
+                    Navigator.pushNamed(context, DisplayEvenementScreen.routeName,
+                        arguments: event)
+                  },
+                  trailing: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 3.0),
+                        child: Text( Datum),
+                      ),
+                      Text( event.city),
+                    ],
+                  ),
+                  isThreeLine: true,
+                );
+
+
+              }),
+        ),
+      ],
+    );
   } else if (snapshot.connectionState == ConnectionState.done &&
       !snapshot.hasData) {
     // Handle no data
@@ -80,6 +174,6 @@ Widget buildEventsList(BuildContext context, AsyncSnapshot<QuerySnapshot> snapsh
     );
   } else {
     // Still loading
-    return CircularProgressIndicator();
+    return Center(child: CircularProgressIndicator());
   }
 }
