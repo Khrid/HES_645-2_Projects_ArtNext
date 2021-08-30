ca9b03aa1b6fe1f039a1a2361acb696279158a89
David <david.crittin@students.hevs.ch>
2021-08-30 21:19:51 +0200

    Attendees list with "more" button if >3 attendees

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:artnext/models/myuser.dart';
 import 'package:artnext/pages/events/DisplayEvenementScreen.dart';
+import 'package:artnext/pages/events/ListAttendees.dart';
 import 'package:artnext/pages/events/ListEventsScreen.dart';
 import 'package:artnext/pages/events/manage/CreateEvenementScreen.dart';
 import 'package:artnext/pages/events/manage/MyEvents.dart';
@@ -149,6 +150,7 @@ class _AppState extends State<App> with WidgetsBindingObserver {
                   CreateEvenementScreen(),
               UpdateEvenementScreen.routeName: (context) =>
                   UpdateEvenementScreen(),
+              ListAttendees.routeName: (context) => ListAttendees(),
               UserInfo.routeName: (context) => UserInfo(),
               MyEvents.routeName: (context) => MyEvents()
             }));
--- lib/pages/events/ListAttendees.dart
+++ lib/pages/events/ListAttendees.dart
@@ -1,28 +1,20 @@
-
 import 'package:artnext/models/event.dart';
 import 'package:artnext/models/myuser.dart';
 import 'package:artnext/pages/common/MyAppBar.dart';
 import 'package:artnext/pages/common/MyDrawer.dart';
-import 'package:artnext/pages/events/DisplayEvenementScreen.dart';
-import 'package:artnext/widget/readTimeStamp.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'manage/CreateEvenementScreen.dart';
 
 export 'ListEventsScreen.dart';
 
 class ListAttendees extends StatelessWidget {
   static const routeName = '/events/event/display/attendees';
 
-
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<MyUser?>(context);
-    final event = ModalRoute
-        .of(context)!
-        .settings
-        .arguments as Event;
+    final event = ModalRoute.of(context)!.settings.arguments as Event;
     print("ListAttendees - user = " + user.toString() + " " + event.toString());
     return Scaffold(
       backgroundColor: Colors.brown[100],
@@ -43,39 +35,71 @@ class ListAttendees extends StatelessWidget {
           ),
         ],
       ),
-      drawer: MyDrawer(""),
+      //drawer: MyDrawer(""),
     );
   }
 }
 
-Widget buildAttendeesList(BuildContext context,
-      AsyncSnapshot<DocumentSnapshot> snapshot) {
-    if (snapshot.hasData) {
-      var event = snapshot.data;
-      Event e = Event.fromJson(event);
-      return Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-                itemCount: e.listAttendees.length,
-                itemBuilder: (context, index) {
+Widget buildAttendeesList(
+    BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+  if (snapshot.hasData) {
+    var event = snapshot.data;
+    Event e = Event.fromJson(event);
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+              itemCount: e.listAttendees.length,
+              itemBuilder: (context, index) {
+                return StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection("users")
+                        .doc(e.listAttendees[index].toString())
+                        .snapshots(),
+                    builder: buildAttendeeCard);
+                /*MyUser tmp = MyUser();
+                  tmp.setUid(e.listAttendees[index].toString());
+                  tmp.populateUserInfoFromFirestore();
+                  print(tmp);
                   return Card(
                     elevation: 5,
                     child: ListTile(
                       title: Text(
-                          e.listAttendees[index].toString()),
+                          tmp.firstname + " " + tmp.lastname),
                       leading: SizedBox(
                         height: 100.0,
                         width: 100.0,
                       ),
-                      isThreeLine: true,
+                      //isThreeLine: true,
                     ),
-                  );
-                }),
-          ),
-        ],
-      );
-    } else {
-      return Text("No attendees yet :(");
-    }
+                  );*/
+              }),
+        ),
+      ],
+    );
+  } else {
+    return Text("No attendees yet :(");
   }
+}
+
+Widget buildAttendeeCard(BuildContext context,
+    AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+  if (snapshot.hasData) {
+    var attendee = snapshot.data;
+    return Card(
+      elevation: 5,
+      child: ListTile(
+        title: Text(attendee!["lastname"].toString() +
+            " " +
+            attendee["firstname"].toString()),
+        leading: SizedBox(
+          height: 100.0,
+          width: 100.0,
+        ),
+        //isThreeLine: true,
+      ),
+    );
+  } else {
+    return Column();
+  }
+}
